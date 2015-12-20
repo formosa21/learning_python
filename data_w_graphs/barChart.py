1c46ba5a6d2ef10b9ae31dcd9481130912eef9dc
Kuei-Hsien Chu <guixien@gmail.com>
2015-12-20 01:04:27 -0600

    bar chart

---
+++ data_w_graphs/barChart.py
@@ -0,0 +1,28 @@
+'''
+Example of drawing a horizontal bar chart
+'''
+
+import matplotlib.pyplot as plt
+
+def create_bar_chart(data, labels):
+	#Numbers of bars
+	num_bars = len(data)
+	#This list is the point on the y-axis where each
+	#bar is centered. Here it will be [1,2,3,...]
+	positions = range(1, num_bars+1)
+	plt.barh(positions, data, align='center')
+	#Set the label of each bar
+	plt.yticks(positions, labels)
+	plt.xlabel('Steps')
+	plt.ylabel('Day')
+	plt.title('Number of steps walked')
+	#Turns on the grid which may assit in visual estimation
+	plt.grid()
+	plt.show()
+
+if __name__ == '__main__':
+	#Number of steps I walked during the past week
+	steps = [6534, 7000, 8900, 10768, 3467, 11045, 5095]
+	#Corresponding days
+	labels = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+	create_bar_chart(steps, labels)
