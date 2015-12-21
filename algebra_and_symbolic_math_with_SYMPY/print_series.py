3fa2a4a30dd83a2d9a1ed3d321e295d8fab0de09
Kuei Hsien Chu <guixien@gmail.com>
2015-12-21 01:31:46 -0600

    algebra

---
+++ algebra_and_symbolic_math_with_SYMPY/print_series.py
@@ -0,0 +1,19 @@
+'''
+print the series x^n/n
+'''
+
+from sympy import Symbol, pprint, init_printing
+
+def print_series(n):
+	# Initialize printing system with reverse order
+	init_printing(order = 'rev-lex')
+	
+	x = Symbol('x')
+	series = x
+	for i in range(2, n+1):
+		series = series + (x**i)/i
+	pprint(series)
+
+if __name__ == '__main__':
+	n = input('Enter the number of terms you want in the series:')
+	print_series(int(n))
